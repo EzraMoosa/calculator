de4d0eba1daec37af53f0ff7c0a096b27b0323ba
ezra <ezrae18@gmail.com>
2025-02-03 12:28:53 +0200

    Get display and button containers and intialize buttons in array

---
+++ main.js
@@ -0,0 +1,14 @@
+// Input display
+const display = document.getElementById("display");
+
+// Buttons container
+const buttons = document.getElementById("buttons");
+
+// Calculator buttons in an array
+buttonArray = [
+    "7", "8", "9", "/",
+    "4", "5", "6", "*",
+    "1", "2", "3", "-",
+    "0", ".", "=", "+",
+    "C"
+];
